f45851f42816bf185dc0bfd8e5af6a94425575cb
nabilaammouri <ammourinabila349@gmail.com>
2024-09-25 17:57:01 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 
 int main()
 {
-    float celsuis , kelvin ;
-    printf("Veuillez entrez la temperature en c :");
-    scantf("%f" , & celsuis);
-    kelvin = celsuis + 273.15 ;
-    printf("la temperature en kelvin est : %2f",kelvin);
+    int S , n ;
+    S = 0 ;
+    for(n=1,n<=10,n++){
+            S=S+n;
+    }
+    printf("La somme des 10 premiers entiers positifs : %d" ,S);
     return 0;
 
 }
