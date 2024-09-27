d696970eeb812f0ba00e0bea49b5d8b46cbfe7f0
nabilaammouri <ammourinabila349@gmail.com>
2024-09-27 10:24:21 +0200

    challenge

---
+++ main 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int A  ,B;
+    printf("Veuillez entrer la valeur de A :");
+    scanf("%d" ,&A);
+    printf("Veuillez entrer la valeur de B :");
+    scanf("%d" ,&B);
+       if (A*B>0)
+    printf("A et B ont meme le signe :");
+       else
+    printf("A et B deux nombre diffirente :");
+    return 0;
+
+}
