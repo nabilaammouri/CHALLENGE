1fdbfc5e34f9f00967f41ee4ff331a0f3828c0c1
nabilaammouri <ammourinabila349@gmail.com>
2024-09-28 12:15:28 +0200

    challenge

--- exam.c
+++ exam.c
@@ -6,7 +6,7 @@ int main()
     int age ;
        printf("Veuillez entrer l'age :");
        scanf("%d" ,&age);
-    if (age <= 18);
+    if (age <= 18)
        printf("entrer la catagorie est vous n etes pas eligible pour voter");
     else
         printf("entrer la catagorie est vous etes eligible pour voter");
--- les boucles.c
+++ les boucles.c
@@ -1,21 +1,20 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int  N , i ,est premier ;
+    int  N , i , estPremier ;
     printf("veuillez entrer un nombre :");
-    scanf("%d" ,&N).
-    est premier =1;
+    scanf("%d" ,&N);
+    estPremier =1;
     for(i=2;i<=N/2;i++){
             if(N%i==0){
-                    est premier=0;
-            break
+                    estPremier=0;
+    break;
       }
 
     }
-    if(est premier==1)
+    if(estPremier==1)
         printf("%d est un nombre premier" ,N);
     else
         printf("%d est un nombre non premier" ,N);
@@ -28,9 +27,6 @@ int main()
 
 
 
-
-
-**************************************************************
 
 
 
