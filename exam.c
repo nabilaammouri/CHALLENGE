aefe0032b148aa1370f1392b25f42194c9c4781a
nabilaammouri <ammourinabila349@gmail.com>
2024-09-28 10:04:36 +0200

    challenge

---
+++ exam.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age ;
+       printf("Veuillez entrer l'age :");
+       scanf("%d" ,&age);
+    if (age <= 18);
+       printf("entrer la catagorie est vous n etes pas eligible pour voter");
+    else
+        printf("entrer la catagorie est vous etes eligible pour voter");
+    return 0;
